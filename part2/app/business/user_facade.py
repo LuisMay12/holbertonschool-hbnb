58fcaa69fac7b6413622d911da8256e707c2995c
MalachisProducions02 <maalchisproductionsnooficial@gmail.com>
2025-06-14 21:08:03 +0000

    user facade

---
+++ part2/app/business/user_facade.py
@@ -0,0 +1,32 @@
+from app.persistence.repository import memory_repository
+from app.persistence.models.user import user
+
+class UserFacade:
+    def __init__(self):
+        self.repo = memory_repository
+
+    def create_user(self, data):
+        user = User(
+            email=data.get("email")
+            password=data.get("password")
+            first_name=data.get("first_name", "")
+            last_name=data.get("last_name", "")
+        )
+        self.repo.save("User", user)
+        return user
+
+    def get_user(self, user_id):
+        return self.repo.get("User", user_id)
+
+    def get_all_users(self):
+        return self.repo.all("User")
+
+    def update_user(self, user_id, updates):
+        user = self.repo.get("user", user_id)
+        if not user:
+            return None
+        for key in ["fist_name", "last_name", "email", "password"]:
+            if key in updates:
+                setattr(user, key, updates[key])
+        user.update_timestamp()
+        return user
