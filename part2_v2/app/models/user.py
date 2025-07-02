e45585213aae8dc93106fd3884d11c186b15a654
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-01 21:28:13 -0600

    user password

--- part2_v2/app/models/user.py
+++ part2_v2/app/models/user.py
@@ -1,9 +1,25 @@
 from .base_model import BaseModel
 import re
+from app import db, bcrypt
+import uuid
 
 class User(BaseModel):
-    """User model with strict validations"""
-    
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)   
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def hash_password(self, password):
+        """Hash the password before storing it"""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verify the hash password"""
+        return bcrypt.check_password_hash(self.password, password)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.first_name = kwargs.get('first_name', '')
