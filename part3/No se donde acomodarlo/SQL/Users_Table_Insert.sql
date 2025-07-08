77a476c0c65cad5ef6c52f894e5dbc7706ca11b1
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-07 18:14:12 -0600

    SQL Tables

---
+++ part3/No se donde acomodarlo/SQL/Users_Table_Insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (
+id,
+email,
+first_name,
+last_name,
+password,
+is_admin
+) VALUES (
+'36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+'admin@hbnb.io',
+'Admin',
+'HBnB',
+'$2b$12$9aCjJTKqrlRRMk5tfdCo1ehD12v1HZuB2cfhIofHeuEiIhI0kH6Gm',
+TRUE
+);
+
