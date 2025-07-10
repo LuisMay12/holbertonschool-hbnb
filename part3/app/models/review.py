bf94ddcb3f57dca8f7aab9cc9af455d1918ddf01
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-07-10 17:08:32 -0600

    correction

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -4,7 +4,7 @@ from app import db
 class Review(db.Model):
     __tablename__ = 'reviews'
 
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1024), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
 
